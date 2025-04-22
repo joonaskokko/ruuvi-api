95c467c60d7aeb257a0dc72b3cc57284fb461268
Joonas Kokko <joonas@tulikukka.com>
2025-04-22 19:34:48 +0300

    Added more information to current history. Some minor refactoring and more tests.

--- src/models/historyModel.ts
+++ src/models/historyModel.ts
@@ -1,6 +1,10 @@
 import db from '../config/database.ts';
 import { ensureTag, getTags } from '../models/tagModel.ts';
 
+const METRICS = [ 'temperature', 'humidity' ];
+const DEFAULT_TREND_VALUE_AMOUNT = 3;
+const CURRENT_HISTORY_MIN_MAX_HOURS = 12;
+
 export interface History {
 	ruuvi_id?: string;
 	tag_id?: number;
@@ -9,6 +13,13 @@ export interface History {
 	humidity?: number;
 	voltage?: number;
 	battery_low?: boolean;
+}
+
+/**
+ * Utility function to check given metric validity.
+ */
+function isValidMetric(metric) {
+	return METRICS.includes(metric);
 }
 
 export async function saveHistory({ tag_id, ruuvi_id, datetime, temperature, humidity, voltage, battery_low = false }: History): Promise<boolean> {
@@ -31,7 +42,7 @@ export async function saveHistory({ tag_id, ruuvi_id, datetime, temperature, hum
 	return true;
 }
 
-export async function getHistory({ date_start = null, date_end = null, tag_id = null } = {}): Promise<History[]> {
+export async function getHistory({ date_start = null, date_end = null, tag_id = null, limit = null } = {}): Promise<History[]> {
 	if ((date_start && !date_end) || (!date_start && date_end)) throw new Error("Data range must contain start and end date time or neither.");
 	
 	const history: object[] = await db('history')
@@ -43,6 +54,7 @@ export async function getHistory({ date_start = null, date_end = null, tag_id = 
 			}
 		
 			if (tag_id) query.where('tag.id', tag_id);
+			if (limit) query.limit(limit);
 		})
 		.orderBy('history.datetime', 'DESC');
 		
@@ -50,7 +62,7 @@ export async function getHistory({ date_start = null, date_end = null, tag_id = 
 }
 
 export async function getCurrentHistory(): Promise<History[]> {
-	const current: object[] = await db('history')
+	let history: object[] = await db('history')
 		.leftJoin('tag', 'history.tag_id', 'tag.id')
 		.join(
 			db('history')
@@ -66,31 +78,68 @@ export async function getCurrentHistory(): Promise<History[]> {
 		.select('history.*', 'tag.name as tag_name')
 		.orderBy('tag_name', 'ASC');
 	
-		// Get maximum and minimum last 12 hours.
+	// Get maximum and minimum last 12 hours and get direction of temperature and humidity.
+	history = await Promise.all(history.map(async row => {
+		const tag_id = row.tag_id;
+		const date_end = new Date(row.datetime);
+		const date_start = new Date(date_end.getTime() - (CURRENT_HISTORY_MIN_MAX_HOURS * 60 * 60 * 1000)); // Argh.
 		
-		// Get direction of temperature and humidity.
+		row.temperature_min = await getMinOrMaxValueByTag({ tag_id, type: 'min', metric: 'temperature', date_start, date_end });
+		row.temperature_max = await getMinOrMaxValueByTag({ tag_id, type: 'max', metric: 'temperature', date_start, date_end });
+		row.temperature_trend = await getMetricTrendByTag({ tag_id, metric: 'temperature' });
+		row.humidity_min = await getMinOrMaxValueByTag({ tag_id, type: 'min', metric: 'humidity', date_start, date_end });
+		row.humidity_max = await getMinOrMaxValueByTag({ tag_id, type: 'max', metric: 'humidity', date_start, date_end });
+		row.humidity_trend = await getMetricTrendByTag({ tag_id, metric: 'humidity' });
+		
+		return row;
+	}));
 	
-	return current;
+	return history;
 }
 
 // Utility function get maximum value of a metric.
 export async function getMinOrMaxValueByTag({ type, tag_id, metric, date_start, date_end }): number {
-	if (type !== 'min' && type !== 'max') throw new Error("Type needs to be min or max.");
+	if (!['min', 'max'].includes(type)) throw new Error("Type needs to be min or max.");
 	if (!date_start || !date_end) throw new Error("Data range must contain start and end date time.");
+	if (date_start > date_end) throw new Error("Start date cannot be before end date.");
 	if (!tag_id) throw new Error("Missing tag ID.");
 	if (!metric) throw new Error("Missing metric name.");
+	if (metric && !isValidMetric(metric)) throw new Error("Not a valid metric name: " + metric);
 	
+	// TODO: Maybe refactor this to use getHistory instead?
 	const { value }: number = await db('history')
 		.modify(query => {
+			// For clarity, these are separate rows.
 			if (type === 'min') query.min({ value: metric });
 			if (type === 'max') query.max({ value: metric });
 		})
-		.max({ value: metric }) // This resolves to .min or .max.
 		.where('tag_id', tag_id)
 		.whereBetween('datetime', [ date_start, date_end ])
 		.first();
 	
 	return value;
+}
+
+export async function getMetricTrendByTag({ tag_id, metric }): number {
+	if (!tag_id) throw new Error("Missing tag ID.");
+	if (!metric) throw new Error("Missing metric name.");
+	if (metric && !isValidMetric(metric)) throw new Error("Not a valid metric name: " + metric);
+		
+	// Get history and limit it to DEFAULT_TREND_VALUE_AMOUNT.
+	const metric_values: History[] = await getHistory( { tag_id, limit: DEFAULT_TREND_VALUE_AMOUNT })
+	const [ first, second, third ] = metric_values.map(row => row[metric]);
+
+	// Very simple trend comparison.
+	// TODO: Add some sort of tolerance here, eg. 0.05 or something.
+	if (first > second && second > third) {
+		return 1;
+	}
+	else if (first < second && second < third) {
+		return -1;
+	}
+	else {
+		return 0;
+	}
 }
 
 export async function cleanOldHistory(days: number): number {
--- test/test.ts
+++ test/test.ts
@@ -7,129 +7,167 @@ import '../index.ts';
 import * as tagModel from '../src/models/tagModel.ts';
 import * as historyModel from '../src/models/historyModel.ts';
 
-console.log("Creating tags:");
-
-await tagModel.ensureTag({ ruuvi_id: "10:00:00:00:00:00", name: "Vessa" });
-await tagModel.ensureTag({ ruuvi_id: "20:00:00:00:00:00", name: "Olohuone" });
-await tagModel.ensureTag({ ruuvi_id: "30:00:00:00:00:00" });
-
-await tagModel.ensureTag({ ruuvi_id: "C8:9B:06:CC:8C:20", name: "Parveke" });
-await tagModel.ensureTag({ ruuvi_id: "F7:89:9C:1C:39:A7", name: "Vintti" });
-
-console.log("Getting tags:");
-
-console.log(await tagModel.getTagById(1));
-console.log(await tagModel.getTagByRuuviId("20:00:00:00:00:00"));
-console.log(await tagModel.getTags());
-
-console.log("Creating history:");
-
-await historyModel.saveHistory(
-	{
-		tag_id: 1,
-		datetime: new Date("2020-02-02T02:02:02+02:00"),
-		temperature: 15.20,
-		humidity: 73.11,
-		battery_low: false
-	}
-);
-
-await historyModel.saveHistory(
-	{
-		tag_id: 2,
-		datetime: new Date("2020-02-02T02:02:04+02:00"),
-		temperature: 17.30,
-		humidity: 80.13,
-		battery_low: false
-	}
-);
-
-await historyModel.saveHistory(
-	{
-		tag_id: 2,
-		datetime: new Date("2020-02-01T02:02:04+02:00"),
-		temperature: 1.50,
-		humidity: 100,
-		battery_low: false
-	}
-);
-
-await historyModel.saveHistory(
-	{
-		tag_id: 2,
-		datetime: new Date("2020-02-02T23:59:04+02:00"),
-		temperature: 1.50,
-		humidity: 100,
-		battery_low: true
-	}
-);
-
-await historyModel.saveHistory(
-	{
-		tag_id: 3,
-		datetime: new Date("2019-03-01T02:02:04+02:00"),
-		temperature: 25.03,
-		humidity: 1.15151,
-		battery_low: false
-	}
-);
-
-try {
-	console.log(await historyModel.getHistory({ tag_id: 1 }));
-}
-catch (error) {
-	console.error(error);
-}
-
-
-console.log("Only 2020-02-02");
-console.log(await historyModel.getHistory({ date_start: new Date("2020-01-01"), date_end: new Date("2025-01-01") }));
-
 const url = 'http://localhost:8080';
 
-await fetch(url + '/history')
-	.then(response => response.json())
-	.then(data => console.log(data))
-	.catch(error => console.error('Error:', error)
-);
+async function testCreateTags() {
+	console.log("Creating tags:");
+	await tagModel.ensureTag({ ruuvi_id: "10:00:00:00:00:00", name: "Vessa" });
+	await tagModel.ensureTag({ ruuvi_id: "20:00:00:00:00:00", name: "Olohuone" });
+	await tagModel.ensureTag({ ruuvi_id: "30:00:00:00:00:00" });
 
-// Read GW data files.
-const ruuviGwPayload = await fs.readFile('./test/ruuvi-gateway.json', 'utf-8');
-const ruuviGwPayloadWizard = await fs.readFile('./test/ruuvi-gateway-wizard.json', 'utf-8');
+	await tagModel.ensureTag({ ruuvi_id: "C8:9B:06:CC:8C:20", name: "Parveke" });
+	await tagModel.ensureTag({ ruuvi_id: "F7:89:9C:1C:39:A7", name: "Vintti" });
+}
 
-// Send a POST request
-const gwResponse = await fetch(url + "/history", {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: ruuviGwPayload
-});
+async function testGetTags() {
+	console.log("Getting tags:");
+	console.log(await tagModel.getTagById(1));
+	console.log(await tagModel.getTagByRuuviId("20:00:00:00:00:00"));
+	console.log(await tagModel.getTags());
+}
 
-console.log(await gwResponse.json());
+async function testCreateHistory() {
+	console.log("Creating history:");
 
-// Send a POST request
-const gwResponseWizard = await fetch(url + "/history", {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: ruuviGwPayloadWizard
-});
+	await historyModel.saveHistory(
+		{
+			tag_id: 1,
+			datetime: new Date("2020-02-02T02:02:02+02:00"),
+			temperature: 15.20,
+			humidity: 73.11,
+			battery_low: false
+		}
+	);
 
-console.log(await gwResponseWizard.json());
+	await historyModel.saveHistory(
+		{
+			tag_id: 2,
+			datetime: new Date("2020-02-02T02:02:04+02:00"),
+			temperature: 1.52,
+			humidity: 80.13,
+			battery_low: false
+		}
+	);
+	
+	await historyModel.saveHistory(
+		{
+			tag_id: 2,
+			datetime: new Date("2020-02-01T20:02:04+02:00"),
+			temperature: 1.03,
+			humidity: 70.11,
+			battery_low: false
+		}
+	);
 
-await fetch(url + "/history")
-	.then(response => response.json())
-	.then(data => console.log(data))
-	.catch(error => console.error('Error:', error)
-);
+	await historyModel.saveHistory(
+		{
+			tag_id: 2,
+			datetime: new Date("2020-02-01T02:02:04+02:00"),
+			temperature: 1.43,
+			humidity: 100,
+			battery_low: false
+		}
+	);
 
-console.log(await historyModel.getMinOrMaxValueByTag({ type: 'max', tag_id: 2, metric: 'temperature', date_start: new Date('2020-02-01T00:00:00+02:00'), date_end: new Date('2020-02-03T00:00:00+02:00') })); // 17.2
+	await historyModel.saveHistory(
+		{
+			tag_id: 2,
+			datetime: new Date("2020-02-02T23:59:04+02:00"),
+			temperature: 1.57,
+			humidity: 100,
+			battery_low: true
+		}
+	);
 
-const aggregateDate = new Date('2020-02-02');
+	await historyModel.saveHistory(
+		{
+			tag_id: 3,
+			datetime: new Date("2019-03-01T02:02:04+02:00"),
+			temperature: 25.03,
+			humidity: 1.15151,
+			battery_low: false
+		}
+	);
+}
 
-console.log(await historyModel.aggregateHistory(aggregateDate));
+async function testGetHistory() {
+	console.log("Test getting history of tag 1:");
+	console.log(await historyModel.getHistory({ tag_id: 1 }));
+	console.log("Only 2020-02-02");
+	console.log(await historyModel.getHistory({ date_start: new Date("2020-01-01"), date_end: new Date("2025-01-01") }));
+}
 
+async function testGetHistoryFromServer() {
+	console.log("Get server history:");
+	await fetch(url + '/history')
+		.then(response => response.json())
+		.then(data => console.log(data))
+		.catch(error => console.error('Error:', error)
+	);
+}
+
+async function testCurrentHistoryFromServer() {
+	console.log("Get server current history:");
+	await fetch(url + '/current')
+		.then(response => response.json())
+		.then(data => console.log(data))
+		.catch(error => console.error('Error:', error)
+	);
+}
+
+async function testRuuviGWPaylods() {
+	console.log("Test saving GW payloads:");
+	// Read GW data files.
+	const ruuviGwPayload = await fs.readFile('./test/ruuvi-gateway.json', 'utf-8');
+	const ruuviGwPayloadWizard = await fs.readFile('./test/ruuvi-gateway-wizard.json', 'utf-8');
+
+	// Send a POST request
+	const gwResponse = await fetch(url + "/history", {
+	    method: 'POST',
+	    headers: {
+	        'Content-Type': 'application/json'
+	    },
+	    body: ruuviGwPayload
+	});
+
+	console.log(await gwResponse.json());
+
+	// Send a POST request
+	const gwResponseWizard = await fetch(url + "/history", {
+	    method: 'POST',
+	    headers: {
+	        'Content-Type': 'application/json'
+	    },
+	    body: ruuviGwPayloadWizard
+	});
+
+	console.log(await gwResponseWizard.json());
+}
+
+async function testGetMinOrMax() {
+	console.log('Test max value: ' + await historyModel.getMinOrMaxValueByTag({ type: 'max', tag_id: 2, metric: 'temperature', date_start: new Date('2020-02-01T00:00:00+02:00'), date_end: new Date('2020-02-03T00:00:00+02:00') })); // 17.2
+	console.log('Test min value: ' + await historyModel.getMinOrMaxValueByTag({ type: 'min', tag_id: 2, metric: 'temperature', date_start: new Date('2020-02-01T00:00:00+02:00'), date_end: new Date('2020-02-03T00:00:00+02:00') })); // 1.43
+}
+
+async function testAggregateData() {
+	const aggregateDate = new Date('2020-02-02');
+
+	console.log(await historyModel.aggregateHistory(aggregateDate));
+}
+
+async function testHistoryTrend() {
+	console.log(await historyModel.getMetricTrendByTag({ tag_id: 2, metric: 'temperature' }))
+}
+
+await testCreateTags();
+await testGetTags();
+await testCreateHistory();
+await testGetHistory();
+await testGetHistoryFromServer();
+await testCurrentHistoryFromServer();
+await testRuuviGWPaylods();
+await testGetMinOrMax();
+await testAggregateData();
+await testHistoryTrend();
 
 process.exit();
